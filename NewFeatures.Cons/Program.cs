db2032ad1fcb1b895e9b6c45f8d3f644e9e1d01a
wilschok <william.schokkele@howest.be>
2020-02-08 13:47:31 +0100

    Add project files.

---
+++ NewFeatures.Cons/Program.cs
@@ -0,0 +1,20 @@
+﻿using NewFeatures.Domain;
+using System;
+
+namespace NewFeatures.Cons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point = new Point();
+            point.X = 3;
+            point.Y = 2;
+
+            // not possible due to member access
+            // point.Distance = 5.0; 
+
+            Console.WriteLine(point);
+        }
+    }
+}
